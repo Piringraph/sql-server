4962ed9d7cfefb30acd20ad2d482306ff1d3958c
Pirin <chuckdanorris123@gmail.com>
2022-12-24 23:21:44 -0400

    added database connection

--- src/controllers/language.controller.js
+++ src/controllers/language.controller.js
@@ -1,5 +1,10 @@
-const getLanguages = (req, res) => {
-    res.json("Pirin-Graph");
+import { getConnection } from "./../databases/database";
+
+const getLanguages = async (req, res) => {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT id, name, programmers FROM language");
+    console.log(result);
+    res.json (result);
 };
 
 export const methods = {
